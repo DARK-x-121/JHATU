121404ec8a0b216fdb1e959c9a1d10906beddd1a
Amit <bhaktaa922@gmail.com>
2026-08-26 16:03:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,8 @@ def call_groq_api(api_key, messages):
         "Content-Type": "application/json"
     }
     payload = {
-                "model": "llama3-8b-8192",
+                "model": "llama-3.3-70b-versatile",
+        
         
         "messages": messages,
         "temperature": 0.6,
